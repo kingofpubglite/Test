2f13a8ce954eb971b15757232e949ff61d2a9e39
kingofpubglite <Mahipannu84@gmail.com>
2025-07-24 10:23:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ TOKEN = '7738364591:AAF2GDNgVTqrIwqR6sFHza27SmpOQ6frQ6E'
 
 # MongoDB URI for connecting to a MongoDB database.
 # This is a connection string that allows your bot to access a MongoDB cluster.
-MONGO_URI = 'mongodb+srv://manishsingh22222sarwan:manishsingh22222sarwan@cluster0.g4jzd3v.mongodb.net'
+MONGO_URI = 'mongodb+srv://sushil:sushil@lgcyalex8565.9q6nj.mongodb.net'
 
 # Enter your Admin's Chat's ID here, Get it from @missRose_bot by typing /id
 # The admin IDs refer to the Telegram user IDs who have administrative access to the bot and its operations.
-ADMIN_IDS = [1957692504, 1957692504]  # Replace with the actual user IDs of your bot admins.
+ADMIN_IDS = [1957692504, 1419969308]  # Replace with the actual user IDs of your bot admins.
